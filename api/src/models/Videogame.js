a02c499447f386c012f5082fba5dab28fbfe5189
Davico84 <davicova84@gmail.com>
2023-03-14 23:22:54 -0500

    BD_DONE

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey:true,
+      defaultValue:DataTypes.UUIDV4,
+    },
+    nombre: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
+    },
+    descripcion:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    plataformas: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image:{
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    fecLan:{
+      type:DataTypes.DATEONLY,
+      allowNull: false
+    },
+    rating:{
+      type:DataTypes.FLOAT,
+      allowNull: false
+    },
+    create:{
+      type: DataTypes.BOOLEAN,
+      defaultValue:true
     },
   });
 };
